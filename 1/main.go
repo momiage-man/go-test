3491753c74724fc18417a0f1402fca3eaca9bc5b
shingo suzuki <s_shingo@icloud.com>
2016-01-23 18:20:18 +0900

    1.1 update

--- 1/main.go
+++ 1/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-"log"
-"net/http"
+	"log"
+	"net/http"
 )
 
-func main (){
-
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+      <html>
+        <head>
+          <title>チャット</title>
+        </head>
+        <body>
+          チャットしましょう！
+        </bod>
+      </html>
+      `))
+	})
+	// web サーバを開始します
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
